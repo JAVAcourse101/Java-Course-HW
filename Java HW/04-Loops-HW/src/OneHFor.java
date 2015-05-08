54d2a5edad881bd934e658f59107660c79c9c19f
BorisGichev <boris.gichev@gmail.com>
2015-05-08 13:59:08 +0300

    Loops

---
+++ Java HW/04-Loops-HW/src/OneHFor.java
@@ -0,0 +1,13 @@
+public class OneHFor {
+
+	public static void main(String[] args) {
+		for(int i=1; i <= 100; i++){
+
+			if ((i % 7 == 0) || (i % 11 == 0)){
+				System.out.println(i);
+			}
+			
+		}
+	}
+
+}
